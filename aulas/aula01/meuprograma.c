7469e4f965973f5f128341c45aaf8a0814f6b913
Christiele123 <25529779-Christiele123@users.noreply.replit.com>
2023-08-24 00:48:17 +0000

    arquivos da aula01

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,15 @@
+/*
+Autor: Christiele
+Data: 23/08/2023
+Programa que exibe Olá Pessoal
+  */
+
+#include <stdio.h>
+#include <math.h>
+
+//funcao pricipal
+int main() {
+   printf("Ola pessoal!\n");
+   printf("A raiz quadrada de 2 é %f\n", sqrt(2));
+   return 0;
+}
