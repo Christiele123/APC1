cfe6b2121ac84d88025ef0c81b81cb12e88421da
Christiele123 <142457805+Christiele123@users.noreply.github.com>
2023-08-31 00:49:11 +0000

    codigo da aula02 e da pratica 02

---
+++ praticas/pratica02/questao02.c
@@ -0,0 +1,18 @@
+/*
+ Faça um programa em C que calcule a área do triangulo (a base x altura/2). Considere base e altura números inteiros maior que 0.
+   */
+#include <stdio.h>
+
+int main(){
+   int base = 5;
+   int altura = 10;
+  
+   float area_do_triangulo = base * altura /2.0f;
+   
+  printf("\n a área do triangulo é %f\n",area_do_triangulo);
+  
+  return 0;
+
+}
+
+ 
